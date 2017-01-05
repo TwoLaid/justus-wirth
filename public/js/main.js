9e67ad1de631f5acc73b7b0dda8de866f17c870f
Timo Djürken <timo.djuerken@gmail.com>
2017-01-05 15:24:44 +0100

    active navigation

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,39 @@
 
 console.log('init')
 
+function scrollTo(element) {
+  console.log('target:', element.offsetTop);
+  var targetY = element.offsetTop - 80;
+  var startTime = Date.now();
+  var duration = 300;
+  const start = document.body.scrollTop;
+
+
+  function animatedScroll() {
+    var now = Date.now();
+    var time = Math.min(1, ((now - startTime) / duration));
+    // var timeFunction = easings[easing](time);
+    document.body.scrollTop = (time * (targetY - start)) + start;
+
+    if (document.body.scrollTop === targetY) {
+      return;
+    }
+    requestAnimationFrame(animatedScroll);
+  }
+
+  animatedScroll();
+}
+
 var stickyNav = document.querySelector('nav.navigation-level-2');
+var navElements = document.querySelectorAll('nav.navigation-level-2 .nav-item a');
+
+for (var i = 0; i < navElements.length; i++) {
+  navElements[i].addEventListener('click', function(e) {
+    e.preventDefault();
+    scrollTo(document.querySelector(this.hash));
+  });
+}
+
 
 window.onscroll = function() {
   var scrollY = window.scrollY;
@@ -11,26 +43,29 @@ window.onscroll = function() {
   } else {
     stickyNav.classList.remove('sticky');
   }
+
+  var maxOnScreen = 0;
+  var maxOnScreenTarget = navElements[0];
+
+  // focus on top half of screen
+  var windowHeight = window.outerHeight/2;
+
+  for (var i = 0; i < navElements.length; i++) {
+    var target = navElements[i].hash;
+    var rect = document.querySelector(target).getBoundingClientRect();
+    var onScreen = (Math.min(rect.bottom, windowHeight) - Math.max(0, rect.top)) / windowHeight;
+    if (onScreen > maxOnScreen) {
+      maxOnScreen = onScreen;
+      maxOnScreenTarget = navElements[i];
+    }
+  }
+
+  for (var i = 0; i < navElements.length; i++) {
+    var e = navElements[i];
+    if (e == maxOnScreenTarget) {
+      e.parentElement.classList.add('active');
+    } else {
+      e.parentElement.classList.remove('active');
+    }
+  }
 }
-
-  // waypoints
-  new Waypoint({
-    element: document.getElementById('music'),
-    handler: function(direction) {
-      console.log('music', direction);
-    }
-  });
-
-  new Waypoint({
-    element: document.getElementById('gigs'),
-    handler: function(direction) {
-      console.log('gigs', direction);
-    }
-  });
-
-  new Waypoint({
-    element: document.getElementById('about'),
-    handler: function(direction) {
-      console.log('about', direction);
-    }
-  });
