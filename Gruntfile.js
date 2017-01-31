b116df472d69230dda4ea6d1a5403188b442b9c6
Timo Djürken <timo.djuerken@gmail.com>
2017-01-31 15:06:38 +0100

    add image optimizer

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,4 @@
-module.exports = function(grunt) {
-  require('jit-grunt')(grunt);
-
+module.exports = function (grunt) {
   grunt.initConfig({
     less: {
       development: {
@@ -23,8 +21,25 @@ module.exports = function(grunt) {
           nospawn: true
         }
       }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'public/img/',
+          src: ['*.{jpg,png}'],
+          dest: 'public/img/'
+        }]
+      }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.registerTask('default', ['less', 'watch']);
+  // grunt.registerTask('less', []);
+  // grunt.registerTask('watch', []);
+  // grunt.registerTask('minimize');
+  // grunt.registerTask('minimize')
 };
